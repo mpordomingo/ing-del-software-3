afb9397250083c414025912cc6b2bc1c18850752
Martin Pordomingo <--global>
2020-06-19 17:21:18 -0300

    serializer error handling added.

--- chronos/views.py
+++ chronos/views.py
@@ -23,7 +23,11 @@ def task_list(request):
     elif request.method == 'POST':
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            try:
+                serializer.save()
+            except Exception as e:
+                return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
